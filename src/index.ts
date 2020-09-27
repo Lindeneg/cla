e17d411531cc16c0bda47045d54253e2fe477f75
Christian <christianlindeneg@gmail.com>
2020-09-27 23:49:21 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,5 @@
-import {
-    EventSummary
-} from './modules/eventSummary';
-import {
-    errorMessage
-} from './modules/constants';
-
-class Webalyze {
-
-    isRunning: boolean;
-    start: () => void;
-    stop: () => void;
-
-    constructor() {
-        let _eventSummary: EventSummary = new EventSummary({
-            server: 'http://localhost:5000/api',
-            token: ''
-        });
-
-        this.isRunning = false;
-
-        this.start = (): void => {
-            if (!this.isRunning) {
-                this.isRunning = true;
-                _eventSummary.start();
-            }
-        }
-
-        this.stop = (): void => {
-            if (this.isRunning) {
-                this.isRunning = false;
-                _eventSummary.stop();
-            }
-        }
-    }
-}
-
-if (typeof globalThis.Storage === "undefined") {
-    console.error(errorMessage.storage);
-} else if (typeof globalThis.MutationObserver === "undefined") {
-    console.error(errorMessage.observer);
-} else {
-    globalThis.Webalyze = new Webalyze();
-}
+const main = (msg: string): number => {
+  console.log(msg);
+  return 0;
+};
+main("Hello there!");
